476fe5e7a98a552781dc37f58b2a97a95d8a8245
Jacob Schaupp <jschaup2@rockets.utoledo.edu>
2020-04-23 22:21:13 -0400

    Additional Classes

---
+++ ChoholicsAnonymous/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChoholicsAnonymous
+{
+    class User
+    {
+        bool manager { get; set; }
+        bool provider { get; set; }
+        string userID { get; set; }
+    }
+}
