187ceed37933450bd46171c45bba703eb27044e9
Jacob Schaupp <jschaup2@rockets.utoledo.edu>
2020-04-29 19:41:30 -0400

    Session Class and Serivce Class

--- ChoholicsAnonymous/Session.cs
+++ ChoholicsAnonymous/Session.cs
@@ -8,5 +8,24 @@ namespace ChoholicsAnonymous
 {
     class Session
     {
+        int sessionID { get; set; }
+        int serviceType { get; set; }
+        int memberID { get; set; }
+        int providerID { get; set; }
+        Date DateOfSession { get; set; }
+        Date TimeOfCreation { get; set; } //time when the session object is created 
+        string comments { get; set; }
+
+        public Session()
+        {
+            String currDate              = DateTime.Now.ToString();
+            DateTime dateValue           = (Convert.ToDateTime(currDate.ToString()));
+            this.DateOfSession           = new Date();
+            this.TimeOfCreation          = new Date();
+            this.TimeOfCreation.Day      = dateValue.Day;
+            this.TimeOfCreation.Month    = dateValue.Month;
+            this.TimeOfCreation.Year     = dateValue.Year; 
+        }
+       
     }
 }
